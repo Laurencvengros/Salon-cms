ecd13e542a4f7b67942a82df9e9ecdc83f5ec183
kelsieszost <owner@kelsieszost.design>
2022-12-19 13:57:25 -0500

    calendar component routes not active

---
+++ client/src/components/Calendar.js
@@ -0,0 +1,30 @@
+import { Scheduler } from "@aldabil/react-scheduler";
+import React from "react";
+
+function Schedule() {
+  return (
+    <React.Fragment>
+      <div className="calendarContainer">
+        <Scheduler
+          view="month"
+          events={[
+            {
+              event_id: 1,
+              title: "Event 1",
+              start: new Date("2021/5/2 09:30"),
+              end: new Date("2021/5/2 10:30"),
+            },
+            {
+              event_id: 2,
+              title: "Event 2",
+              start: new Date("2021/5/4 10:00"),
+              end: new Date("2021/5/4 11:00"),
+            },
+          ]}
+        />
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default Schedule;
