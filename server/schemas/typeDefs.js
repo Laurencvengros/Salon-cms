5cb7012ab475b4779c1ea3dec8132633f9f48c53
Lauren <l.cvengros@icloud.com>
2022-12-20 09:47:59 -0500

    fixed an error in package json with installing dependencies

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,34 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+    type Query{
+        me: User
+    }
+    type Clients {
+        _id: ID!
+        firstName: String!
+        lastName: String!
+        email: String!
+        phone: Number
+    }
+    type User {
+        _id: ID!
+        name: String!
+        email: String!
+        salonName: String!
+        clients: [Clients]
+    }
+    type Event {
+        event_id: ID!
+        title: String!
+        start: Date!
+        end: Date!
+        client: [Clients]
+    }
+    type Auth {
+        token: ID!
+        user: User
+      }
+`
+
+module.exports = typeDefs;
