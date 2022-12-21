f77a990eadd7d345ffed989b47ea53433e1dced6
kelsieszost <owner@kelsieszost.design>
2022-12-20 20:59:02 -0500

    created addclient form, now storing data as an object for db

--- client/src/components/Clients.js
+++ client/src/components/Clients.js
@@ -2,26 +2,14 @@ import React from "react";
 import Card from "react-bootstrap/Card";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
+import AddClient from "./AddClient";
 
 function Clients() {
   return (
-    <Row xs={1} md={2} className="g-4">
-      {Array.from({ length: 4 }).map((_, idx) => (
-        <Col>
-          <Card>
-            <Card.Body>
-              <Card.Title>Client Name</Card.Title>
-              <Card.Text>
-                Email: <a href="#clientEmail">client@email.com</a>
-              </Card.Text>
-              <Card.Text>
-                Phone: <a href="#clientPhone">123-456-7890</a>
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-      ))}
-    </Row>
+    <div>
+      <button className="addClientBtn">Add New Client</button>
+      <AddClient />
+    </div>
   );
 }
 
---
+++ client/src/components/addClient.js
@@ -0,0 +1,50 @@
+import { useState } from "react";
+
+const AddClient = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newClient = { firstName, lastName, email, phone };
+    console.log(newClient);
+  };
+
+  return (
+    <div className="create">
+      <h2>Add A New Client</h2>
+      <form onSubmit={handleSubmit}>
+        <label>First Name:</label>
+        <input
+          type="text"
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+          required
+        />
+        <label>Last Name:</label>
+        <input
+          type="text"
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
+          required
+        />
+        <label>Email:</label>
+        <input
+          type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <label>Phone:</label>
+        <input
+          type="text"
+          value={phone}
+          onChange={(e) => setPhone(e.target.value)}
+        />
+        <button>Add Client</button>
+      </form>
+    </div>
+  );
+};
+export default AddClient;
