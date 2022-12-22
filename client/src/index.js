4f20e05ddfa6fc8427da5a2857d731680216a25b
kelsieszost <owner@kelsieszost.design>
2022-12-22 18:52:46 -0500

    front end routing in progress

--- client/src/index.js
+++ client/src/index.js
@@ -3,4 +3,5 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
+
 ReactDOM.render(<App />, document.getElementById("root"));
---
+++ client/src/utils/API.js
@@ -0,0 +1,9 @@
+export const addClient = (addClient) => {
+  return fetch("/api/clients", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(addClient),
+  });
+};
