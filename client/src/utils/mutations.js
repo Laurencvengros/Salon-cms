b93a55a02f730f6f2b9b44448ce6a53989f5e4af
Lauren <l.cvengros@icloud.com>
2022-12-27 14:13:43 -0500

    fix typos

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -2,7 +2,7 @@ import { gql } from '@apollo/client';
 
 export const ADD_CLIENT = gql`
   mutation addClient($userId: ID!, $firstName: String!, $lastName: String!, $email: String, $phone: Int) {
-    addClient(userID: $userID, firstName: $firstName, lastName: $lastName, email: $email, phone: $phone) {
+    addClient(userId: $userId, firstName: $firstName, lastName: $lastName, email: $email, phone: $phone) {
       _id
       firstName
       lastName
