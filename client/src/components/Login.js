1e167f7d0250de1092d53916833c14d6718ebba0
Michaela Wells <mgwells012@gmail.com>
2022-12-30 16:52:30 -0500

    changes in login and signup to get backend to run

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,3 +1,114 @@
+
+
+// const Login = (props) => {
+//   const [userData, setUserData] = useState({ email: '', password: '' });
+//   const [login, { error, data }] = useMutation(LOGIN_USER);
+
+//   // update state based on form input changes
+//   const handleInputChange = (event) => {
+//     const { name, value } = event.target;
+
+//     setUserData({
+//       ...userData,
+//       [name]: value,
+//     });
+//   };
+
+//   // submit form
+//   const handleFormSubmit = async (event) => {
+//     event.preventDefault();
+//     console.log(userData);
+//     try {
+//       const { data } = await login({
+//         variables: { ...userData},
+//       });
+
+//       Auth.login(data.login.token);
+//       console.log('success!')
+//     } catch (e) {
+//       console.error(e);
+//       console.log('error logging in')
+//     }
+
+
+//     // clear form values
+//     setUserData({
+//       email: '',
+//       password: '',
+//     });
+//   };
+
+//   return (
+//   <React.Fragment>
+//     <Container className="container">
+//       <main>
+//         <div className='card'>
+//             <h3 className="p-2">Login</h3>
+//             <div className="card-body mb-3">
+//               {data ? (
+//                 <Link to="/home">back to the homepage!</Link>
+//               ) : (
+//                 <form onSubmit={handleFormSubmit}>
+
+//                   <div className="mb-3">
+//                     <label> Email Address: </label>
+//                       <input
+//                         className="form-input form-control"
+//                         placeholder="Your email"
+//                         name="email"
+//                         type="email"
+//                         value={userData.email}
+//                         onChange={handleInputChange}
+//                       />
+//                   </div>
+
+//                   <div className="mb-3">
+//                     <label> Password: </label>
+//                   <input
+//                     className="form-input form-control"
+//                     placeholder="******"
+//                     name="password"
+//                     type="password"
+//                     value={userData.password}
+//                     onChange={handleInputChange}
+//                   />
+//                   </div>
+
+//                   <div className="d-grid">
+//                     <button
+//                       className="btn btn-info"
+//                       style={{ cursor: 'pointer' }}
+//                       type="submit">
+//                       Submit
+//                     </button>
+//                   </div>
+
+//                 </form>
+                
+//               )}
+
+//               {error && (
+//                 <div className="my-3 p-3 bg-danger text-white">
+//                   {error.message}
+//                 </div>
+//               )}
+//             </div>
+
+            
+//               <p style={{ paddingLeft:'15pt'}}>
+//                 Don't have an account? <a href="/signup"> sign up here</a>
+//               </p>
+            
+
+//           </div>
+//       </main>
+//     </Container>
+// </React.Fragment>
+//   );
+// };
+
+// export default Login;
+
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useMutation } from '@apollo/client';
@@ -5,7 +116,6 @@ import { LOGIN_USER } from '../utils/mutations';
 
 
 import Auth from '../utils/auth';
-import Container from 'react-bootstrap/esm/Container';
 
 const Login = (props) => {
   const [userData, setUserData] = useState({ email: '', password: '' });
@@ -31,10 +141,10 @@ const Login = (props) => {
       });
 
       Auth.login(data.login.token);
-      console.log('success!')
+      console.log('success')
     } catch (e) {
       console.error(e);
-      console.log('error logging in')
+      console.log('error loggin in')
     }
 
 
@@ -46,103 +156,56 @@ const Login = (props) => {
   };
 
   return (
-  <React.Fragment>
-    <Container className="container">
-      <main>
-        <div className='card'>
-            <h3 className="p-2">Login</h3>
-            <div className="card-body mb-3">
-              {data ? (
-                <Link to="/home">back to the homepage!</Link>
-              ) : (
-                <form onSubmit={handleFormSubmit}>
+    <main className="flex-row justify-center mb-4">
+      <div className="col-12 col-lg-10">
+        <div className="card">
+          <h4 className="card-header bg-dark text-light p-2">Login</h4>
+          <div className="card-body">
+            {data ? (
+              <Link to="/">back to the homepage.</Link>
+            ) : (
+              
+              <form onSubmit={handleFormSubmit}>
+                <input
+                  className="form-input"
+                  placeholder="Your email"
+                  name="email"
+                  type="email"
+                  value={userData.email}
+                  onChange={handleInputChange}
+                />
+                <input
+                  className="form-input"
+                  placeholder="******"
+                  name="password"
+                  type="password"
+                  value={userData.password}
+                  onChange={handleInputChange}
+                />
+                <button
+                  className="btn btn-block btn-info"
+                  style={{ cursor: 'pointer' }}
+                  type="submit"
+                >
+                  Submit
+                </button>
+              </form>
+              
+            )}
 
-                  <div className="mb-3">
-                    <label> Email Address: </label>
-                      <input
-                        className="form-input form-control"
-                        placeholder="Your email"
-                        name="email"
-                        type="email"
-                        value={userData.email}
-                        onChange={handleInputChange}
-                      />
-                  </div>
-
-                  <div className="mb-3">
-                    <label> Password: </label>
-                  <input
-                    className="form-input form-control"
-                    placeholder="******"
-                    name="password"
-                    type="password"
-                    value={userData.password}
-                    onChange={handleInputChange}
-                  />
-                  </div>
-
-                  <div className="d-grid">
-                    <button
-                      className="btn btn-info"
-                      style={{ cursor: 'pointer' }}
-                      type="submit">
-                      Submit
-                    </button>
-                  </div>
-
-                </form>
-                
-              )}
-
-              {error && (
-                <div className="my-3 p-3 bg-danger text-white">
-                  {error.message}
-                </div>
-              )}
-            </div>
-
-            
-              <p style={{ paddingLeft:'15pt'}}>
-                Don't have an account? <a href="/signup"> sign up here</a>
-              </p>
-            
-
+            {error && (
+              <div className="my-3 p-3 bg-danger text-white">
+                {error.message}
+              </div>
+            )}
           </div>
-      </main>
-    </Container>
-</React.Fragment>
+          <p className="forgot-password text-right">
+              Don't have an account? <a href="/signup"> sign up here</a>
+            </p>
+        </div>
+      </div>
+    </main>
   );
 };
 
 export default Login;
-
-//       <main>
-//       <form>
-//           <div className="mb-3">
-//             <label>Email address</label>
-//             <input
-//               type="email"
-//               className="form-control"
-//               placeholder="Enter email"
-//             />
-//           </div>
-
-
-//           <div className="mb-3">
-//             <label>Password</label>
-//             <input
-//               type="password"
-//               className="form-control"
-//               placeholder="Enter password"
-//             />
-//           </div>
-
-//           <div className="d-grid">
-
-
-//           </div>
-//         </form>
-//         </main>
-// };
-
-// export default Login;
--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,10 +1,134 @@
+
+// const SignUp = () => {
+
+//   const [userData, setUserData] = useState({ 
+//     name: '', 
+//     email: '', 
+//     password: '', 
+//     salonName: ''
+//   });
+  
+//     const[ addUser, {error, data}] = useMutation(ADD_USER);
+
+//   const handleInputChange = (event) => {
+//     const { name, value } = event.target;
+
+//     setUserData({...userData, [name]: value});
+//   };
+
+//   const handleFormSubmit = async (event) => {
+//     event.preventDefault();
+//     console.log(userData)
+
+
+//     try {
+//       const { data } =await addUser({variables: {...userData}});
+//       console.log(data)
+//       Auth.login(data.addUser.token);
+//     }catch(error){
+//       console.error(error)
+//     }
+
+//     setUserData({
+//       name: '',
+//       email: '',
+//       password: '',
+//       salonName: '',
+//     });
+//   };
+
+
+//    return (
+//     <React.Fragment>
+//       <Container className="container">
+//         <main>
+//           <div className="card">
+//             <h3 className="p-2">Sign Up</h3>
+//             <div className="card-body mb-3">
+//               {data ? (
+//                 <p>
+//                   Success!
+//                 </p>
+//               ) : (
+//                 <form onSubmit={handleFormSubmit}>
+
+//                   <div className="mb-3">
+//                     <input
+//                       className="form-input form-control"
+//                       placeholder="Your name"
+//                       name="name"
+//                       type="text"
+//                       value={userData.name}
+//                       onChange={handleInputChange}
+//                     />
+//                   </div>
+
+//                   <div className="mb-3">
+//                   <input
+//                     className="form-input form-control"
+//                     placeholder="Your Salon Name"
+//                     name="salonName"
+//                     type="text"
+//                     value={userData.salonName}
+//                     onChange={handleInputChange}
+//                   />
+//                   </div>
+
+//                   <div className="mb-3">
+//                   <input
+//                     className="form-input form-control"
+//                     placeholder="Your email"
+//                     name="email"
+//                     type="email"
+//                     value={userData.email}
+//                     onChange={handleInputChange}
+//                   />
+//                   </div>
+
+//                   <div className="mb-3">
+//                   <input
+//                     className="form-input form-control"
+//                     placeholder="******"
+//                     name="password"
+//                     type="password"
+//                     value={userData.password}
+//                     onChange={handleInputChange}
+//                   />
+//                   </div>
+
+//                   <div className="d-grid">
+//                   <button
+//                     className="btn btn-info"
+//                     style={{ cursor: 'pointer' }}
+//                     type="submit"
+//                   >
+//                     Submit
+//                   </button>
+//                   </div>
+//                 </form>
+//               )}
+
+//               {error && (
+//                 <div className="my-3 p-3 bg-danger text-white">
+//                   {error.message}
+//                 </div>
+//               )}
+//             </div>
+//           </div>
+//         </main>
+//       </Container>
+//     </React.Fragment>
+//   );
+// };
+
+// export default SignUp;
+
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Auth from '../utils/auth';
 
 import { useMutation } from '@apollo/client';
 import { ADD_USER } from '../utils/mutations';
-import { Container } from 'react-bootstrap';
 
 
 
@@ -49,85 +173,68 @@ const SignUp = () => {
 
 
    return (
-    <React.Fragment>
-      <Container className="container">
-        <main>
-          <div className="card">
-            <h3 className="p-2">Sign Up</h3>
-            <div className="card-body mb-3">
-              {data ? (
-                <p>
-                  Success!
-                </p>
-              ) : (
-                <form onSubmit={handleFormSubmit}>
+    <main className="flex-row justify-center mb-4">
+      <div className="col-12 col-lg-10">
+        <div className="card">
+          <h4 className="card-header bg-dark text-light p-2">Sign Up</h4>
+          <div className="card-body">
+            {data ? (
+              <p>
+                Success!
+              </p>
+            ) : (
+              <form onSubmit={handleFormSubmit}>
+                <input
+                  className="form-input"
+                  placeholder="Your name"
+                  name="name"
+                  type="text"
+                  value={userData.name}
+                  onChange={handleInputChange}
+                />
+                <input
+                  className="form-input"
+                  placeholder="Your Salon Name"
+                  name="salonName"
+                  type="text"
+                  value={userData.salonName}
+                  onChange={handleInputChange}
+                />
+                <input
+                  className="form-input"
+                  placeholder="Your email"
+                  name="email"
+                  type="email"
+                  value={userData.email}
+                  onChange={handleInputChange}
+                />
+                <input
+                  className="form-input"
+                  placeholder="******"
+                  name="password"
+                  type="password"
+                  value={userData.password}
+                  onChange={handleInputChange}
+                />
+                <button
+                  className="btn btn-block btn-info"
+                  style={{ cursor: 'pointer' }}
+                  type="submit"
+                >
+                  Submit
+                </button>
+              </form>
+            )}
 
-                  <div className="mb-3">
-                    <input
-                      className="form-input form-control"
-                      placeholder="Your name"
-                      name="name"
-                      type="text"
-                      value={userData.name}
-                      onChange={handleInputChange}
-                    />
-                  </div>
-
-                  <div className="mb-3">
-                  <input
-                    className="form-input form-control"
-                    placeholder="Your Salon Name"
-                    name="salonName"
-                    type="text"
-                    value={userData.salonName}
-                    onChange={handleInputChange}
-                  />
-                  </div>
-
-                  <div className="mb-3">
-                  <input
-                    className="form-input form-control"
-                    placeholder="Your email"
-                    name="email"
-                    type="email"
-                    value={userData.email}
-                    onChange={handleInputChange}
-                  />
-                  </div>
-
-                  <div className="mb-3">
-                  <input
-                    className="form-input form-control"
-                    placeholder="******"
-                    name="password"
-                    type="password"
-                    value={userData.password}
-                    onChange={handleInputChange}
-                  />
-                  </div>
-
-                  <div className="d-grid">
-                  <button
-                    className="btn btn-info"
-                    style={{ cursor: 'pointer' }}
-                    type="submit"
-                  >
-                    Submit
-                  </button>
-                  </div>
-                </form>
-              )}
-
-              {error && (
-                <div className="my-3 p-3 bg-danger text-white">
-                  {error.message}
-                </div>
-              )}
-            </div>
+            {error && (
+              <div className="my-3 p-3 bg-danger text-white">
+                {error.message}
+              </div>
+            )}
           </div>
-        </main>
-      </Container>
-    </React.Fragment>
+        </div>
+      </div>
+    </main>
   );
 };
 
