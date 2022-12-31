ae304dae75b5c1a002deeb3bf5fda107544f3096
Michaela Wells <mgwells012@gmail.com>
2022-12-30 22:26:41 -0500

    log out button added and styled in nav bar - working, name populates in nav bar from log in information, in the nav bar when not logged in only sign up /sign in shows up and nav bar populates (and works) once logged in. all components have been styled

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,118 +1,9 @@
-
-
-// const Login = (props) => {
-//   const [userData, setUserData] = useState({ email: '', password: '' });
-//   const [login, { error, data }] = useMutation(LOGIN_USER);
-
-//   // update state based on form input changes
-//   const handleInputChange = (event) => {
-//     const { name, value } = event.target;
-
-//     setUserData({
-//       ...userData,
-//       [name]: value,
-//     });
-//   };
-
-//   // submit form
-//   const handleFormSubmit = async (event) => {
-//     event.preventDefault();
-//     console.log(userData);
-//     try {
-//       const { data } = await login({
-//         variables: { ...userData},
-//       });
-
-//       Auth.login(data.login.token);
-//       console.log('success!')
-//     } catch (e) {
-//       console.error(e);
-//       console.log('error logging in')
-//     }
-
-
-//     // clear form values
-//     setUserData({
-//       email: '',
-//       password: '',
-//     });
-//   };
-
-//   return (
-//   <React.Fragment>
-//     <Container className="container">
-//       <main>
-//         <div className='card'>
-//             <h3 className="p-2">Login</h3>
-//             <div className="card-body mb-3">
-//               {data ? (
-//                 <Link to="/home">back to the homepage!</Link>
-//               ) : (
-//                 <form onSubmit={handleFormSubmit}>
-
-//                   <div className="mb-3">
-//                     <label> Email Address: </label>
-//                       <input
-//                         className="form-input form-control"
-//                         placeholder="Your email"
-//                         name="email"
-//                         type="email"
-//                         value={userData.email}
-//                         onChange={handleInputChange}
-//                       />
-//                   </div>
-
-//                   <div className="mb-3">
-//                     <label> Password: </label>
-//                   <input
-//                     className="form-input form-control"
-//                     placeholder="******"
-//                     name="password"
-//                     type="password"
-//                     value={userData.password}
-//                     onChange={handleInputChange}
-//                   />
-//                   </div>
-
-//                   <div className="d-grid">
-//                     <button
-//                       className="btn btn-info"
-//                       style={{ cursor: 'pointer' }}
-//                       type="submit">
-//                       Submit
-//                     </button>
-//                   </div>
-
-//                 </form>
-                
-//               )}
-
-//               {error && (
-//                 <div className="my-3 p-3 bg-danger text-white">
-//                   {error.message}
-//                 </div>
-//               )}
-//             </div>
-
-            
-//               <p style={{ paddingLeft:'15pt'}}>
-//                 Don't have an account? <a href="/signup"> sign up here</a>
-//               </p>
-            
-
-//           </div>
-//       </main>
-//     </Container>
-// </React.Fragment>
-//   );
-// };
-
-// export default Login;
-
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useMutation } from '@apollo/client';
 import { LOGIN_USER } from '../utils/mutations';
+import Container from 'react-bootstrap/esm/Container';
+
 
 
 import Auth from '../utils/auth';
@@ -156,10 +47,11 @@ const Login = (props) => {
   };
 
   return (
+    <Container>
     <main className="flex-row justify-center mb-4">
       <div className="col-12 col-lg-10">
         <div className="card">
-          <h4 className="card-header bg-dark text-light p-2">Login</h4>
+          <h4 className="card-header p-2" style={{backgroundColor:'#B19C8D', color:'white', borderColor:'#B19C8D'}}>Login</h4>
           <div className="card-body">
             {data ? (
               <Link to="/">back to the homepage.</Link>
@@ -167,7 +59,7 @@ const Login = (props) => {
               
               <form onSubmit={handleFormSubmit}>
                 <input
-                  className="form-input"
+                  className="form-input form-control mb-3"
                   placeholder="Your email"
                   name="email"
                   type="email"
@@ -175,7 +67,7 @@ const Login = (props) => {
                   onChange={handleInputChange}
                 />
                 <input
-                  className="form-input"
+                  className="form-input form-control mb-3"
                   placeholder="******"
                   name="password"
                   type="password"
@@ -184,7 +76,7 @@ const Login = (props) => {
                 />
                 <button
                   className="btn btn-block btn-info"
-                  style={{ cursor: 'pointer' }}
+                  style={{ cursor: 'pointer', backgroundColor:'#B19C8D', borderColor:'#B19C8D', color:'white' }}
                   type="submit"
                 >
                   Submit
@@ -199,12 +91,13 @@ const Login = (props) => {
               </div>
             )}
           </div>
-          <p className="forgot-password text-right">
+          <p className="forgot-password text-right" style={{paddingLeft:'10pt'}}>
               Don't have an account? <a href="/signup"> sign up here</a>
             </p>
         </div>
       </div>
     </main>
+    </Container>
   );
 };
 
