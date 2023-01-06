297f34af7f16e959d1fc199efb6e3b04a25173ea
Lauren <l.cvengros@icloud.com>
2023-01-05 21:25:20 -0500

    redeploy

--- server/server.js
+++ server/server.js
@@ -18,29 +18,29 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
-   
-  }
-  
-  app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client'));
-    
-  })
-  
+  app.use(express.static(path.join(__dirname, '../client/build')));
 
-  const startApolloServer = async (typeDefs, resolvers) => {
-    await server.start();
-    server.applyMiddleware({ app });
-    
-    db.once('open', () => {
-      app.listen(PORT, () => {
-        console.log(`API server running on port ${PORT}!`);
-        console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-      })
+}
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+
+})
+
+
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
+
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
     })
-    db.on('error', (err) => {
-      console.error('error connecting to MongoDB', err);
-    });
-  };
-  
-    startApolloServer(typeDefs, resolvers);
+  })
+  db.on('error', (err) => {
+    console.error('error connecting to MongoDB', err);
+  });
+};
+
+startApolloServer(typeDefs, resolvers);
